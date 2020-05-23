bfd53a6e0d11070fd747175f82037565d51338ea
J Kaufman <jtkaufman737@gmail.com>
2020-05-23 17:05:36 -0400

    add route outline

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+
+import googlemaps
+import os
+import re
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        # Login/signup page
+
+    @app.route('/login')
+    def login():
+
+    @app.route('/signup')
+    def signup():
+        # new users
+
+    @app.route('/subscribe')
+        # can structure this to allow delete or addition
+
+    @app.route('/location')
+       # I was thinking if it gets a query string it can filter, if not it returns the whole list   
+
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app().run()
