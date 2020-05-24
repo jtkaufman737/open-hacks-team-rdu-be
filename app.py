80ed56ad789799732895c0ab9ebebe5b227ee61d
Richard Thomas <richard.thomas@duke.edu>
2020-05-23 21:38:55 -0400

    Added email and SMS sending logic

--- app.py
+++ app.py
@@ -6,17 +6,50 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from twilio.rest import Client
+
 
 def create_app():
     app = Flask(__name__)
     load_dotenv()
 
+    def send_sms(phone):
+        account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+        auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body='this is a test',
+            messaging_service_sid=os.getenv('TWILIO_SERVICE_SID'),
+            to=phone
+        )
+        print(message)
+        return message.sid
+
+    def send_email(email):
+        message = Mail(
+            from_email='notifications@coronalert.app',
+            to_emails=email,
+            subject='CoronAlert Daily Notifications',
+            html_content='this is a test'
+        )
+        try:
+            sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+            response = sg.send(message)
+        except Exception as e:
+            print(e)
+
+        return "success"
+
+    def send_notifications():
+        pass
+
     @app.route('/login')
     def login():
+        pass
 
     @app.route('/signup')
     def signup():
-        # new users
+        pass
 
     @app.route('/subscribe')
         # can structure this to allow delete or addition
@@ -26,24 +59,6 @@ def create_app():
 
     return app
 
-def send_notifications():
-    pass
-
-def send_sms(phone):
-    pass
-
-def send_email(email, subscriptions):
-    message = Mail(
-        from_email='notifications@coronalert.app',
-        to_emails=email,
-        subject='CoronAlert Daily Notifications',
-        html_content='this is a test'
-    )
-    try:
-        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
-        response = sg.send(message)
-    except Exception as e:
-        print(e.message)
 
 if __name__ == '__main__':
     create_app().run()
